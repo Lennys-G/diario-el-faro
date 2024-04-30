82a00cf6f1c0c20a4f9b4dcf14b2cc2073147067
Lennys-G <lsgm2002@gmail.com>
2024-04-30 08:44:08 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://diario-el-faro-production-f959.up.railway.app/'),
 
     /*
     |--------------------------------------------------------------------------
